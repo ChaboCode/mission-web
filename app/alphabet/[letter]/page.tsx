ce184846d25db4525d8cacb7128fd8a800340b09
ChaboCode <kaerdos.dev@protonmail.com>
2023-09-06 23:50:16 -0600

    MinIO

---
+++ app/alphabet/[letter]/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+import Image from "next/image"
+import { useEffect } from "react"
+
+
+function AlphabetLetter({params}: {params: {letter: string}}) {
+    const {letter} = params
+
+    useEffect(() => {})
+
+    return (
+        <>
+        <Image src={`http://${process.env.NEXT_PUBLIC_MINIO_ENDPOINT}/web/letter_${letter}.jpg`} alt="XD" height={100} width={300}/>
+        Displaying {letter}
+        </>
+    )
+}
+
+export default AlphabetLetter
--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "localhost", "mission-minio.flycast"
+        ]
+    },
+}
 
 module.exports = nextConfig
